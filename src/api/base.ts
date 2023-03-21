12ec6a47bb0f3903ba4ca130912c5cd5133a4415
Dawid Pachciarek <pachciar3@gmail.com>
2023-03-21 17:32:58 +0100

    feat: refactor api integration

--- src/api/base.ts
+++ src/api/base.ts
@@ -1,18 +1,23 @@
 const API_URL = process.env.NEXT_PUBLIC_API_URL;
 
 async function request(endpoint: string, method: string = 'GET') {
-  const config: RequestInit = {
-    method,
-    headers: {
-      'Content-type': 'application/json',
-    },
-  };
-  const url = `${API_URL}${endpoint}`;
-  const response = await fetch(url, config);
-  if (response.ok) {
-    return response.json();
-  } else {
-    throw Error(response.statusText);
+  try {
+    const config: RequestInit = {
+      method,
+      headers: {
+        'Content-type': 'application/json',
+      },
+    };
+    const url = `${API_URL}${endpoint}`;
+    const response = await fetch(url, config);
+    if (response.ok) {
+      return response.json();
+    } else {
+      throw new Error(`Bad response code: ${response.status}`);
+    }
+  } catch (e) {
+    console.error(e);
+    throw new Error(`Error from ${request.name}: ${e}`);
   }
 }
 
--- src/api/methods/getAllFilms/getAllFilms.ts
+++ src/api/methods/getAllFilms/getAllFilms.ts
@@ -1,21 +1,16 @@
+import swapi from '@/api/base';
+
 import { FilmsResponse } from './FilmsResponse';
 import { allFilmsResponseSchema } from './validationSchema';
 
 async function getAllFilms() {
   try {
-    const response = await fetch(
-      `${process.env.NEXT_PUBLIC_API_URL}/films?format=json`,
-    );
-    if (response.ok) {
-      const data = await response.json();
-      await allFilmsResponseSchema.validate(data);
-      return data as FilmsResponse;
-    } else {
-      throw new Error(`Bad response code: ${response.status}`);
-    }
+    const data = await swapi.get('/films?format=json');
+    await allFilmsResponseSchema.validate(data);
+    return data as FilmsResponse;
   } catch (e: unknown) {
     console.error(e);
-    throw new Error(`Unknown error: ${e}`);
+    throw new Error(`Error from ${getAllFilms.name}: ${e}`);
   }
 }
 
